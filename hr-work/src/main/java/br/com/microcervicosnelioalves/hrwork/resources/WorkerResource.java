19979bb187811170d6379deef85c3aeda062334f
anonymous <jcolivati@gmail.com>
2021-04-11 20:18:05 -0300

    hr-worker,hr-payroll implementation postgres and Ribbon

--- hr-work/src/main/java/br/com/microcervicosnelioalves/hrwork/resources/WorkerResource.java
+++ hr-work/src/main/java/br/com/microcervicosnelioalves/hrwork/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package br.com.microcervicosnelioalves.hrwork.resources;
 
 import br.com.microcervicosnelioalves.hrwork.entities.Worker;
 import br.com.microcervicosnelioalves.hrwork.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,11 @@ import java.util.List;
 @RequestMapping(value = "/worker")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment environment;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -27,6 +35,7 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        logger.info("PORT = " + environment.getProperty("local.server.port"));
         Worker object = repository.findById(id).get();
         return new ResponseEntity<>(object, HttpStatus.OK);
     }
