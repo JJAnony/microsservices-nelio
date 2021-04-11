2f14e452e392f83f38df3b57d4f46647d802173f
anonymous <jcolivati@gmail.com>
2021-04-11 13:33:37 -0300

    hr-payroll implementation Feign Client

---
+++ hr-payroll/src/main/java/br/com/microcervicosnelioalves/hrpayroll/clients/WorkerClient.java
@@ -0,0 +1,17 @@
+package br.com.microcervicosnelioalves.hrpayroll.clients;
+
+import br.com.microcervicosnelioalves.hrpayroll.entities.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/worker")
+public interface WorkerClient {
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Worker> findById(@PathVariable Long id);
+
+}
--- hr-payroll/src/main/java/br/com/microcervicosnelioalves/hrpayroll/services/WorkerService.java
+++ hr-payroll/src/main/java/br/com/microcervicosnelioalves/hrpayroll/services/WorkerService.java
@@ -1,27 +1,18 @@
 package br.com.microcervicosnelioalves.hrpayroll.services;
 
+import br.com.microcervicosnelioalves.hrpayroll.clients.WorkerClient;
 import br.com.microcervicosnelioalves.hrpayroll.entities.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class WorkerService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerClient client;
 
     public Worker getWorker(long workerId) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", Long.toString(workerId));
-        return restTemplate.getForObject(workerHost+ "/worker/{id}", Worker.class, uriVariables);
+        return client.findById(workerId).getBody();
     }
 
 }
