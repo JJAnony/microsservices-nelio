fa12396e5d12680ece8323e43bc36a48585b3da9
anonymous <jcolivati@gmail.com>
2021-04-13 23:45:50 -0300

    Configuring Eureka server

--- hr-eurecaserver/src/main/java/br/com/microcervicosnelioalves/hreurecaserver/HrEurecaserverApplication.java
+++ hr-eurecaserver/src/main/java/br/com/microcervicosnelioalves/hreurecaserver/HrEurecaserverApplication.java
@@ -2,7 +2,9 @@ package br.com.microcervicosnelioalves.hreurecaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurecaserverApplication {
 
