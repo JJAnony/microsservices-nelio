56a51639e0786b07e221d9685a09c26dd0e0973a
anonymous <jcolivati@gmail.com>
2021-04-13 23:36:18 -0300

    Project hr-eureka-server created

---
+++ hr-eurecaserver/src/main/java/br/com/microcervicosnelioalves/hreurecaserver/HrEurecaserverApplication.java
@@ -0,0 +1,13 @@
+package br.com.microcervicosnelioalves.hreurecaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurecaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurecaserverApplication.class, args);
+	}
+
+}
---
+++ hr-eurecaserver/src/test/java/br/com/microcervicosnelioalves/hreurecaserver/HrEurecaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microcervicosnelioalves.hreurecaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurecaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
